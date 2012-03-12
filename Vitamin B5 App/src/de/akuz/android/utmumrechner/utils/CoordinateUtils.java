c86f266e7f993262b2b1e58d8ecf2f03920b388a
Till Klocke <till.klocke@itemis.de>
2012-03-12 09:45:43 +0100

    Added new methods to CoordinateUtils to parse different GPS formats

--- Vitamin B5 App/src/de/akuz/android/utmumrechner/utils/CoordinateUtils.java
+++ Vitamin B5 App/src/de/akuz/android/utmumrechner/utils/CoordinateUtils.java
@@ -7,25 +7,65 @@ import uk.me.jstott.jcoord.UTMRef;
 import uk.me.jstott.jcoord.datum.WGS84Datum;
 
 public class CoordinateUtils {
-	
-	public static String latLonToMGRS(double latitude, double longitude){
+
+	public final static String LOCATION_UTILS_PROVIDER = "calculatedLocation";
+
+	public static String latLonToMGRS(double latitude, double longitude) {
 		LatLng gps = new LatLng(latitude, longitude);
 		gps.toDatum(WGS84Datum.getInstance());
 		UTMRef utmRef = gps.toUTMRef();
 		MGRSRef mgrsRef = new MGRSRef(utmRef);
 		return mgrsRef.toString();
 	}
-	
-	public static String locationToMGRS(Location location){
+
+	public static String locationToMGRS(Location location) {
 		return latLonToMGRS(location.getLatitude(), location.getLongitude());
 	}
-	
-	public static Location mgrsToLocation(String mgrs){
-		Location location = new Location("calculatedLocation");
+
+	public static Location mgrsToLocation(String mgrs) {
+		Location location = new Location(LOCATION_UTILS_PROVIDER);
 		MGRSRef mgrsRef = new MGRSRef(mgrs);
 		location.setLatitude(mgrsRef.toLatLng().getLatitude());
 		location.setLongitude(mgrsRef.toLatLng().getLongitude());
 		return location;
+	}
+
+	public static Location parseDMM(String latitude, String longitude) {
+		double lat = parseDMM(latitude);
+		double lon = parseDMM(longitude);
+		return buildLocationObject(lat, lon);
+	}
+
+	private static double parseDMM(String in) {
+		String[] parts = in.split(" ");
+		double fullDegress = Double.parseDouble(parts[0].trim());
+		double minutes = Double.parseDouble(parts[1].trim());
+		return (fullDegress + (minutes / 60.0));
+	}
+
+	public static Location parseDMS(String latitude, String longitude) {
+		double lat = parseDMS(latitude);
+		double lon = parseDMS(longitude);
+		return buildLocationObject(lat, lon);
+	}
+
+	private static double parseDMS(String in) {
+		String[] parts = in.split(" ");
+		double fullDegrees = Double.parseDouble(parts[0].trim());
+		double minutes = Double.parseDouble(parts[1].trim());
+		double seconds = Double.parseDouble(parts[2].trim());
+		
+		seconds = seconds / 60.0d;
+		minutes = ((minutes + seconds )/ 60.0d);
+
+		return (fullDegrees + minutes);
+	}
+	
+	private static Location buildLocationObject(double latitude, double longitude){
+		Location l = new Location(LOCATION_UTILS_PROVIDER);
+		l.setLatitude(latitude);
+		l.setLongitude(longitude);
+		return l;
 	}
 
 }
