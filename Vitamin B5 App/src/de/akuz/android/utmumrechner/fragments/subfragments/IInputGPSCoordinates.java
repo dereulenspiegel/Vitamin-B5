c5a408ea71b80c444400e33c20c38273fe56d9a6
Till Klocke <till.klocke@itemis.de>
2012-03-14 15:05:06 +0100

    -Fixed library path -Deleted unnecessary files

--- Vitamin B5 App/src/de/akuz/android/utmumrechner/fragments/subfragments/IInputGPSCoordinates.java
+++
@@ -1,8 +0,0 @@
-package de.akuz.android.utmumrechner.fragments.subfragments;
-
-import android.location.Location;
-
-public interface IInputGPSCoordinates {
-
-	public Location getLocationFromInput();
-}
